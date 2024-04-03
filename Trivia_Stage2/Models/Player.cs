6c99a11cc21afa920d96e77c8b8027ad230558fd
User <User@LAB3-3>
2024-04-03 09:30:06 +0300

    made log in

--- Trivia_Stage2/Models/Player.cs
+++ Trivia_Stage2/Models/Player.cs
@@ -34,3 +34,44 @@ public partial class Player
     [InverseProperty("Players")]
     public virtual Rank? Rank { get; set; }
 }
+
+public class SentPlayer
+{
+    public string email { get; set; }
+    public string password { get; set; }
+}
+
+public class ReturnedPlayer
+{
+    public int playerId { get; set; }
+    public string playerEmail { get; set; }
+    public string playerPassword { get; set; }
+    public string playerName { get; set; }
+    public int playerScore { get; set; }
+    public Playerrank playerRank { get; set; }
+    public QuestionForLogin[] questions { get; set; }
+}
+
+public class Playerrank
+{
+    public int rankId { get; set; }
+    public string rankName { get; set; }
+}
+
+public class QuestionForLogin
+{
+    public int questionId { get; set; }
+    public Questiontopic questionTopic { get; set; }
+    public int questionPlayerId { get; set; }
+    public string questionText { get; set; }
+    public string questionAnswerText { get; set; }
+    public string questionWrongText1 { get; set; }
+    public string questionWrongText2 { get; set; }
+    public string questionWrongText3 { get; set; }
+    public int questionStatusId { get; set; }
+}
+public class Questiontopic
+{
+    public int topicId { get; set; }
+    public string topicName { get; set; }
+}
---
+++ Trivia_Stage2/Services/TriviaService.cs
@@ -0,0 +1,64 @@
+﻿using Trivia_Stage2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Trivia_Stage2.Services
+{
+    public class TriviaService
+    {
+        Player LoggedPlayer;
+        HttpClient httpClient;//אובייקט לשליחת בקשות וקבלת תשובות מהשרת
+
+        JsonSerializerOptions options;//פרמטרים שישמשו אותנו להגדרות הjson
+
+        const string URL = $@"https://qsc714b9-7128.euw.devtunnels.ms/TriviaApi/";//כתובת השרת
+
+        public TriviaService()
+        {
+            //http client
+            httpClient = new HttpClient();
+
+            //options when doing serialization/deserialization
+            options = new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = true,
+                WriteIndented = true
+            };
+        }
+        public async Task<bool> LogPlayer(string email, string password)
+        {
+            try
+            {
+                JsonSerializerOptions options = new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase, PropertyNameCaseInsensitive = true };
+                string jsonString = JsonSerializer.Serialize(new SentPlayer() { email=email, password=password}, options);
+                HttpResponseMessage message = await httpClient.PostAsync(URL + "/Login", new StringContent(jsonString, Encoding.UTF8, "application/json"));
+                if (message.IsSuccessStatusCode)
+                {
+                    ReturnedPlayer p = JsonSerializer.Deserialize<ReturnedPlayer>(await message.Content.ReadAsStringAsync());
+                    LoggedPlayer = new Player()
+                    {
+                        PlayerId = p.playerId,
+                        Email = p.playerEmail,
+                        Password = p.playerPassword,
+                        PlayerName = p.playerName,
+                        Points = p.playerScore,
+                        RankId = p.playerRank.rankId
+                    };
+                }
+                return LoggedPlayer != null;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+
+
+}
